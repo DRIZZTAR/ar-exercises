988488d1adcd52bbb2862c6792ea63540282687e
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-02-01 13:10:05 -0700

    Create employees for store1 and store2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+@store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 40)
+@store2.employees.create(first_name: "Zac", last_name: "Zaddy", hourly_rate: 30)
+@store2.employees.create(first_name: "Zoey", last_name: "Zasser", hourly_rate: 20)
+@store2.employees.create(first_name: "Zebra", last_name: "Zesterfield", hourly_rate: 10)
+
+
