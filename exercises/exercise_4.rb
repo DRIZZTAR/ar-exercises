47cd1eae19741382451cc8dbdbd984a9f6cf6328
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-02-01 13:05:24 -0700

    Complete exercise 3 through 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts "---Mens Stores---"
+@mens_stores.each do |store|
+  puts "Name: #{store.name} Annual Revenue: #{store.annual_revenue}"
+end
+
+puts "---Womens Apparel Stores---"
+@womens_apparel = Store.where(womens_apparel: true)
+
+@womens_apparel.each do |store|
+  if store.annual_revenue < 1000000
+    puts "Name: #{store.name} Annual Revenue: #{store.annual_revenue}"
+  end
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+
+puts ("All revenue 💰#{@total_revenue}")
+
+@average_revenue = Store.average(:annual_revenue)
+
+puts ("Average revenue ⨏#{@average_revenue}")
+
+@stores_over_million = Store.where("annual_revenue > 1000000").size
+
+puts ("Stores with over a million in revenue: #{@stores_over_million}")
